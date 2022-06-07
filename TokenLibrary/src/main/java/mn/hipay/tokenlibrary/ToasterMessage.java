18294ce385344cb140bbe8a0f402c132d4af6ca9
Sukhbat Amraa <sukhee@Sukhbats-Mac-mini.local>
2022-06-07 15:51:44 +0800

    Initial commit

---
+++ TokenLibrary/src/main/java/mn/hipay/tokenlibrary/ToasterMessage.java
@@ -0,0 +1,12 @@
+package mn.hipay.tokenlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+    public static void show(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
