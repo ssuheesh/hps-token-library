3174bb8f2baa6ba077eda91686336a3ac91d973f
Sukhbat Amraa <sukhee@Sukhbats-Mac-mini.local>
2022-07-21 17:36:03 +0800

    remove unnecessary cardadd callback listener

--- TokenLibrary/src/main/java/mn/hipay/tokenlibrary/callback/CardAddListenerCallback.java
+++
@@ -1,7 +0,0 @@
-package mn.hipay.tokenlibrary.callback;
-
-public interface CardAddListenerCallback
-{
-    public void onSuccess(String successMessage);
-    public void onFailure(Throwable throwableError);
-}
